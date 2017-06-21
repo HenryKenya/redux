04fdc95c070d51802edbe346772c0b40a56896ba
Henry <henry.onyango@outlook.com>
2017-06-21 17:51:42 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <p>Hello React</p>
+      </div>
+    );
+  }
+}
+
+export default App;
